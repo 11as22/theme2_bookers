b0ae55e0ae3e23653e25d32b581860d2b70486d9
11as22 <ec2-user@ip-172-31-3-73.ap-northeast-1.compute.internal>
2023-04-07 07:00:06 +0000

    [show 作り中　recouces 定義]theme2_bookers

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   root :to => 'homes#top'
-  get 'books/new'
-  post 'books' => 'books#create'
-  get 'books' => 'books#index'
-  get 'books/show'
-  get 'books/edit'
-
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+#get 'books/new'
+ # post 'books' => 'books#create'
+ # get 'books' => 'books#index', as: 'book'
+ # get 'books/:id' => 'books#show', as: 'book'
+ # get 'books/edit'
