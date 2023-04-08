f3c40681e404c4f4559eea5011a2352a0f545209
11as22 <ec2-user@ip-172-31-3-73.ap-northeast-1.compute.internal>
2023-04-08 09:09:12 +0000

    [error じっそう] theme2_bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,11 +2,15 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
-  
+
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to  book_path(book.id)
+    @books = Book.all
+    @book = Book.new(book_params)
+    if @book.save
+       redirect_to book_path(@book.id)
+    else
+       render :index
+    end
   end
 
   def index
@@ -19,6 +23,25 @@ class BooksController < ApplicationController
   end
 
   def edit
+   @book = Book.find(params[:id])
+  end
+
+
+  def update
+    @books = Book.all
+    @book = Book.find(params[:id])
+
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
 private
